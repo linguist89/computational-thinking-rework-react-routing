684034f7640a6fab403fad29f2c6dd44483408ce
stephan <smuts1989@gmail.com>
2023-07-26 16:00:32 +0200

    Updates

--- src/lessons.js
+++
@@ -1,4 +0,0 @@
-export const lessons = [
-    { 'title': 'Lesson1', 'subtitle': 'Subtitle', 'content': 'This is the content' },
-    { 'title': 'Lesson2', 'subtitle': 'Subtitle', 'content': 'This is the content' }
-];
